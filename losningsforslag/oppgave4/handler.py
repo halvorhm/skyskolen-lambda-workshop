109c22890c8e2af90392efb4d4d145ec46d47bcf
Tobias Skjelvik <32834550+roonskipp@users.noreply.github.com>
2022-06-27 19:13:08 +0200

    add lf oppg4

---
+++ losningsforslag/oppgave4/handler.py
@@ -0,0 +1,14 @@
+import boto3
+from botocore.exceptions import ClientError
+
+def printBuckets(event, context):
+
+    # Retrieve the list of existing buckets
+    s3 = boto3.client('s3')
+    response = s3.list_buckets()
+
+    # Output the bucket names
+    print('Existing buckets:')
+    for bucket in response['Buckets']:
+        print(f'  {bucket["Name"]}')
+
